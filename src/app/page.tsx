3e4cfad496bdc3b96d55b870c9f9ff136e2814e2
bsherbrook <bsherbrook@gmail.com>
2023-10-18 17:57:37 -0500

    start carousel build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
+"use client";
 import Image from 'next/image'
+import {useState} from "react"
 import Header from '@/components/Header'
 import HomeCarousel from '@/components/HomeCarousel'
 import Footer from '@/components/Footer'
 
 export default function Home() {
+  const images = [
+    "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
   return (
     <main>
       <Header/>
       Testing page.tsx
-      <HomeCarousel/>
+      <HomeCarousel
+        images={images}
+      />
       <Footer/>
     </main>
   )
--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -1,12 +1,63 @@
-import '../styles/HomeCarousel.scss'
+import "../styles/HomeCarousel.scss";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import Image from "next/image";
 
-export default function HomeCarousel() {
+export default function HomeCarousel({ images }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    return (
-      <>
-        <div className="home-carousel-box">
-            Testing
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === images.length ? 0 : prevIndex + 1
+    );
+  };
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+  const handleDotClick = (index: any) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <>
+      <div className="home-carousel-box">
+        <div className="carousel-images">
+          <img key={currentIndex} src={images[currentIndex]} />
+          <div className="slide_direction">
+            <div className="left" onClick={handlePrevious}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="20"
+                viewBox="0 96 960 960"
+                width="20"
+              >
+                <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+              </svg>
+            </div>
+            <div className="right" onClick={handleNext}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="20"
+                viewBox="0 96 960 960"
+                width="20"
+              >
+                <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+              </svg>
+            </div>
+          </div>
+          <div className="carousel-indicator">
+            {images.map((_: any, index: any) => (
+              <div
+                key={index}
+                className={`dot ${currentIndex === index ? "active" : ""}`}
+                onClick={() => handleDotClick(index)}
+              ></div>
+            ))}
+          </div>
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  );
+}
